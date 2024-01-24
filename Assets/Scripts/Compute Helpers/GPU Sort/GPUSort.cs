536647dc687498648e55d35dc27277376eb7c68f
mzub <marcin_zub@outlook.com>
2024-01-24 20:47:14 +0100

    Last refactors

--- Assets/Scripts/Compute Helpers/GPU Sort/GPUSort.cs
+++ Assets/Scripts/Compute Helpers/GPU Sort/GPUSort.cs
@@ -23,15 +23,10 @@ public class GPUSort
         ComputeHelper.SetBuffer(sortCompute, indexBuffer, "Entries", calculateOffsetsKernel);
     }
 
-    // Sorts given buffer of integer values using bitonic merge sort
-    // Note: buffer size is not restricted to powers of 2 in this implementation
     public void Sort()
     {
         sortCompute.SetInt("numEntries", indexBuffer.count);
 
-        // Launch each step of the sorting algorithm (once the previous step is complete)
-        // Number of steps = [log2(n) * (log2(n) + 1)] / 2
-        // where n = nearest power of 2 that is greater or equal to the number of inputs
         int numStages = (int)Log(NextPowerOfTwo(indexBuffer.count), 2);
 
         for (int stageIndex = 0; stageIndex < numStages; stageIndex++)
